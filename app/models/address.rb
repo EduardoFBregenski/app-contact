e8d7bc31f675f7ad0742ccdf20e212b7efba2225
EduardoFBregenski <eduardofbregenski@gmail.com>
2023-07-28 09:54:18 -0300

    Add Association and Validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,12 @@
 class Address < ApplicationRecord
+  # Association
   belongs_to :contact
+
+  # Validation  
+  validates :street, presence: true
+  validates :number, presence: true
+  validates :neighborhood, presence: true
+  validates :city, presence: true
+  validates :uf, presence: true
+  validates :cep, presence: true
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,8 @@
 class Contact < ApplicationRecord
+    # Association
+    has_one :address, dependent: :destroy 
+
+    # Validation
+    validates :name, presence: true
+    validates :phone, presence: true
 end
