29828142f231cf6bf6c93966aa018e81c6168faa
EduardoFBregenski <eduardofbregenski@gmail.com>
2023-07-31 14:42:08 -0300

    Add form to include new contacts

--- spec/features/contact_spec.rb
+++ spec/features/contact_spec.rb
@@ -10,4 +10,14 @@ RSpec.feature "Contacts", type: :feature do
     expect(page).to have_content(contact.name)
     expect(page).to have_content(contact.phone)
   end
+
+  scenario '#new' do
+
+   visit new_contact_path
+   expect(page).to have_content("Criar novo contato.")
+   expect(page).to have_field("Name")
+   expect(page).to have_field("Email")
+   expect(page).to have_field("Phone")
+   expect(page).to have_button("Enviar")
+  end 
 end
