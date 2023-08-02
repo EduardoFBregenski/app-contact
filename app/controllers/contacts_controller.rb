64d518c893df9ebbbf600bcc9051ed7088bafee5
EduardoFBregenski <eduardofbregenski@gmail.com>
2023-08-02 15:21:06 -0300

    Add method create

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,4 +7,24 @@ class ContactsController < ApplicationController
     def new
         @contact = Contact.new
     end
+
+    def create
+        @contact = Contact.new(contact_params)
+    
+        respond_to do |format|
+          if @contact.save
+            format.html { redirect_to contact_url(@contact), notice: "Contact was successfully created." }
+            format.json { render :show, status: :created, location: @contact }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @contact.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+    private
+
+    def contact_params
+        params.require(:contact).permit(:name, :email, :phone)
+    end
 end
